3cec99b18a69475c8c65efa307c8d490df837fd3
lexyking <sahiealexandre@hotmail.fr>
2019-09-02 13:27:13 +0100

    create the form for the user details

--- src/Components/UserDetails.jsx
+++ src/Components/UserDetails.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import TextField from 'material-ui/TextField';
@@ -17,11 +17,53 @@ class userDetails extends Component {
     //Destructuring attributes from the props
     const { values, handleChange } = this.props
     return (
-      <div>
-        <h1>User Details</h1>
-      </div >
+      <MuiThemeProvider>
+        <section className="mainSection">
+          <h2>Enter User Details</h2>
+          <TextField
+            hintText="Enter Your Name"
+            floatingLabelText="Name *"
+            onChange={handleChange('name')}
+            defaultValue={values.name}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your Role"
+            floatingLabelText="role *"
+            onChange={handleChange('role')}
+            defaultValue={values.role}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your Email"
+            floatingLabelText="Email *"
+            onChange={handleChange('email')}
+            defaultValue={values.email}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your Password"
+            floatingLabelText="Password *"
+            onChange={handleChange('password')}
+            defaultValue={values.password}
+          />
+          <br />
+          <RaisedButton
+            label="continue"
+            primary={true}
+            style={styles.button}
+            onClick={this.continue}
+          />
+        </section>
+      </MuiThemeProvider>
     )
 
+  }
+}
+
+const styles = {
+  button: {
+    margin: 15
   }
 }
 
