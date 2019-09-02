b619bcb8b11dd6612a21f985ce06f37e56218265
lexyking <sahiealexandre@hotmail.fr>
2019-09-02 13:11:03 +0100

    creating the userdetails component

--- src/Components/UserDetails.jsx
+++ src/Components/UserDetails.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
-function userDetails() {
-  return (
-    <div>
-      <h1>User Details</h1>
-    </div>
-  )
+class userDetails extends Component {
+
+  //Function continue to move to the next tab
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  }
+
+
+  render() {
+    //Destructuring attributes from the props
+    const { values, handleChange } = this.props
+    return (
+      <div>
+        <h1>User Details</h1>
+      </div >
+    )
+
+  }
 }
 
 export default userDetails
